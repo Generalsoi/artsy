2c5775df934af80cb6f4bc0b5094390c84afbd1f
Generalsoi <ikhinobelesuccess@gmail.com>
2022-12-14 23:22:54 +0100

    fix: add folder structure and implement first stage of routing for the artsy project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Homepage } from "./routes/Homepage";
+import { Marketplace } from "./routes/Marketplace";
+import { Auctions } from "./routes/Auctions";
+import { Drop } from "./routes/Drop";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Artsy</h1>
-    </div>
+    <Routes>
+      <Route element={<Homepage />} path="/" />
+      <Route element={<Marketplace />} path="/marketplace" />
+      <Route element={<Auctions />} path="/auctions" />
+      <Route element={<Drop />} path="/drop" />
+    </Routes>
   );
 }
 
---
+++ src/routes/Auctions.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Auctions = () => {
+  return <div>Auctions</div>;
+};
