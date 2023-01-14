1d61cd2b39b37e84dda2704370030778decbdab3
Generalsoi <ikhinobelesuccess@gmail.com>
2023-01-14 01:59:32 +0100

    fix: start implementing the filter section of the marketplace

--- src/components/MarketplaceContents/filter.jsx
+++ src/components/MarketplaceContents/filter.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
-export const Filter = () => {
-  return <div>F</div>;
+export const Filter = ({ filterContent, filterStyles }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const Category = filterContent.ByCategory;
+  console.log(Category);
+
+  return (
+    <div className={filterStyles}>
+      <span className="md:flex items-center gap-4 pb-3 font-extrabold border-l-0 border-t-0 border-r-0 border-b-4 border-[#AFB091]">
+        <HiOutlineAdjustmentsHorizontal className="text-4xl" />
+        <h2 className="md:text-2xl">Filter</h2>
+      </span>
+
+      {/* Implementing the filter content here */}
+      <div className="mt-10">
+        <h3
+          className="flex items-center gap-4 cursor-pointer text-xl font-bold"
+          onClick={handleOpen}
+        >
+          By Category {open ? <IoIosArrowUp /> : <IoIosArrowDown />}
+        </h3>
+
+        <ul>{open && Category?.map((category) => <li>{category}</li>)}</ul>
+      </div>
+    </div>
+  );
 };
--- src/routes/Marketplace.jsx
+++ src/routes/Marketplace.jsx
@@ -5,13 +5,27 @@ import { SearchMktPlace } from "../components/MarketplaceContents/searchMktPlace
 import { Navbar } from "./../components/Navbar";
 
 export const Marketplace = () => {
-  const filterStyles = [];
+  const filterContent = {
+    ByCategory: ["Editorials", "Fashion", "Optics", "Art & Museum", "Nature"],
+
+    ByPrice: [
+      "Below $100.00",
+      "$100.00 - $150.00",
+      "$150.00 - $200.00",
+      "Above $200.00",
+    ],
+
+    "By Artist": [],
+    "Collection Year": [],
+  };
+
+  const filterStyles = `hidden md:block md:w-[15%] font-satoshi`;
   return (
     <>
       <Navbar />
       <SearchMktPlace />
-      <div className="md:flex">
-        <Filter filterStyles={filterStyles} />
+      <div className="md:flex items-start md:gap-8 px-4 md:px-20 py-6 md:py-10">
+        <Filter filterContent={filterContent} filterStyles={filterStyles} />
         <ArtMarketplace />
       </div>
     </>
