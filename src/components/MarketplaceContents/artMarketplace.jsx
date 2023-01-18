6f4faf47808a2b3034abbbf56e102e49e8e4f60c
Generalsoi <ikhinobelesuccess@gmail.com>
2023-01-18 23:02:03 +0100

    fix: implement the products in the market place section

--- src/components/MarketplaceContents/artMarketplace.jsx
+++ src/components/MarketplaceContents/artMarketplace.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export const ArtMarketplace = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const URL =
+      "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/439bc2dd8693b490539eae236918f4a53dd17457/products.json";
+
+    const getProducts = async () => {
+      try {
+        const response = await axios.get(URL);
+        setProducts(response.data.products);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getProducts();
+  }, []);
+  console.log(products);
   return <div>ArtMarketplace</div>;
 };
